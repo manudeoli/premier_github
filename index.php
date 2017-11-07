8e9464a55dd3db7a24c7eaf6fbe0322fae0ec8dc
walma <contact@walma.fr>
2017-11-07 11:09:14 +0100

    ajout h1 et fond rose

--- index.php
+++ index.php
@@ -2,9 +2,28 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Document sans titre</title>
+<title>Salut</title>
+<style type="text/css">
+body{
+	background-color: #DB17C1;
+	color: white;
+	font-family: Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", monospace
+}
+h1{
+	text-align: center;
+	-webkit-transition: all 100ms ease-in 0s;
+	-o-transition: all 100ms ease-in 0s;
+	transition: all 100ms ease-in 0s;
+}
+h1:hover {
+	color: #922729;
+}
+</style>
 </head>
 
 <body>
+
+<h1>Salut, il est pas beau mon 1er github ??</h1>
+
 </body>
 </html>
